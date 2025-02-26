12276d5dcce1e052ae8aa0e5528edc780126c222
MuntahaChowdhury <muntaha0108@gmail.com>
2025-02-25 19:15:51 -0600

    Updated next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['ridbizecom.s3.ap-southeast-1.amazonaws.com'], // Add the S3 bucket domain
+  },
 };
 
 export default nextConfig;
